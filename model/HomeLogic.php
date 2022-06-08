da89d521a3b597cd198977497451f70a00d43941
DeniseVerhoef <553398@edu.rocmn.nl>
2022-06-08 08:35:59 +0200

    fix calendar

--- model/HomeLogic.php
+++ model/HomeLogic.php
@@ -9,8 +9,7 @@ class HomeLogic
 
     public function __construct()
     {
-        //$this->DataHandler = new Datahandler("web0088.zxcs.nl", "mysql", "sderijknl_minecade", "sderijknl_minecade", "stan2022");
-        $this->DataHandler =  new Datahandler("localhost", "mysql", "minecade", "root", "");
+        $this->DataHandler = new Datahandler("web0088.zxcs.nl", "mysql", "sderijknl_minecade", "sderijknl_minecade", "stan2022");
         $this->Display = new Display();
     }
 
