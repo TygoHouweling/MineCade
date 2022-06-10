e4aa0c32d53cad8ab5d6dbef39d0b8f9d0050f9c
StandeMan15 <99266225+StandeMan15@users.noreply.github.com>
2022-06-10 08:44:34 +0200

    sidebar

---
+++ view/components/sidebar.php
@@ -0,0 +1,39 @@
+<?php
+$sidebar = $mainController->collectReadJSON('view/assets/json/sidebar.json'); ?>
+
+<div class="container">
+  <nav class="main-menu">
+    <ul>
+        <li>
+            <a href="http://google.com">
+                <i class="fa fa-home fa-2x"></i>
+                    <span class="nav-text">
+                        Home
+                    </span>
+            </a>   
+        </li>
+
+        <?php foreach ($sidebar as $key => $value) { ?>
+                <li class="has-subnav">
+                    <a class="sidenav" href="<?= $value->url ?>">
+                    <i class="<?= $value->icon ?>"></i>
+                    <span class="nav-text">
+                        <?= $value->name ?>
+                    </span>
+                    </a>
+                </li>
+            <?php } ?>
+        </ul>
+
+        <ul class="logout">
+            <li>
+                <a href="?con=auth&op=logout">
+                        <i class="fa fa-power-off fa-2x"></i>
+                    <span class="nav-text">
+                        Logout
+                    </span>
+                </a>
+            </li>  
+        </ul>
+    </nav>
+</div>
