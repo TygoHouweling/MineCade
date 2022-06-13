41f7ce97f005a11b4616750303dc96df7ce3a368
StandeMan15 <99266225+StandeMan15@users.noreply.github.com>
2022-06-13 14:58:40 +0200

    meh

--- model/UsersLogic.php
+++ model/UsersLogic.php
@@ -10,10 +10,10 @@ class UsersLogic
   }
   public function __destruct(){}
   
-  public function collectReadAllEvents($event_name, $event_desc, $event_date, $event_location, $event_zipcode){
+  public function collectReadAllUsers($firstname, $lastname, $email, $username){
     try {
 
-        $sql = "INSERT INTO `events`(`event_name`, `event_desc`, `event_date`, `event_location`, `event_zipcode`) VALUES ('{$event_name}', '{$event_desc}', '{$event_date}', '{$event_location}', '{$event_zipcode}')";
+        $sql = "INSERT INTO `users`(`firstname`, `lastname`, `email`, `username`) VALUES ('{$firstname}', '{$lastname}', '{$email}', '{$username}')";
         $html = $this->DataHandler->createData($sql);
         $html = $html->FetchAll(PDO::FETCH_ASSOC);
         return $html; 
@@ -23,10 +23,10 @@ class UsersLogic
     }
   }
 
-  public function readEvents($id)
+  public function readUsers($id)
   {
     try {
-      $sql = "SELECT * FROM events WHERE `event_id`='{$id}'";
+      $sql = "SELECT * FROM users WHERE `users_id`='{$id}'";
       $res = $this->DataHandler->readData($sql);
   
       return $res;
@@ -40,10 +40,10 @@ class UsersLogic
   public function readAllEvents($limit, $perPage)
   {
     try {
-      $sql = "SELECT FOUND_ROWS() as total FROM events";
+      $sql = "SELECT FOUND_ROWS() as total FROM users";
       $res1 = $this->DataHandler->countPages($sql, $perPage);
 
-      $sql = "SELECT event_id as id, event_name, event_date, event_desc, event_location, event_zipcode FROM events LIMIT $limit, $perPage";
+      $sql = "SELECT users_id as id, firstname, lastname, email, username FROM events LIMIT $limit, $perPage";
       $res2 = $this->DataHandler->readsData($sql);
 
       $res2 = $res2->FetchAll(PDO::FETCH_ASSOC);
@@ -56,6 +56,42 @@ class UsersLogic
       }
 
   }
+
+  public function updateEvent($id, $firstname, $lastname, $email, $username) {
+
+    try{
+
+      // if($event_image != ""){
+      //   $image = $this->FileUpload->ImageUpload($_FILES['file']['name']);
+      // }else{
+      //   $sql = "SELECT event_image FROM `events` WHERE `event_id`='{$id}'";
+      //   $result = $this->DataHandler->readData($sql);
+      //   $row = $result->fetch(PDO::FETCH_ASSOC);
+      //   $image = $row['event_image'];
+      // }
+
+      $sql = "UPDATE `users` SET `firstname`='{$firstname}', `lastname`='{$lastname}', `email`='{$email}', `username`='{$username}' WHERE `users_id`='{$id}'";
+      $results = $this->DataHandler->updateData($sql);
+      return $results;
+
+    } catch (Exception $e){
+      throw $e;
+    }
+  }
+
+  public function deleteEvent($id)
+  {
+    try{
+
+      $sql = "DELETE FROM `events` WHERE `event_id`='{$id}'";
+      $result = $this->DataHandler->deleteData($sql);
+      return $result;
+
+    }catch(Exception $e){
+      throw $e;
+    }
+  }
 }
- 
+
+  
 ?>
